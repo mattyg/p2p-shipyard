3992db113704503d98c312b3f7c5089123e208c9
guillem.cordoba <guillem.cordoba@gmail.com>
2024-07-09 17:30:06 +0200

    Removed release signing from example

--- examples/end-user-happ/src-tauri/gen/android/app/build.gradle.kts
+++ examples/end-user-happ/src-tauri/gen/android/app/build.gradle.kts
@@ -14,10 +14,6 @@ val tauriProperties = Properties().apply {
     }
 }
 
-val keyPropertiesFile = rootProject.file("key.properties")
-val keyProperties = Properties()
-keyProperties.load(FileInputStream(keyPropertiesFile))
-
 android {
     compileSdk = 34
     namespace = "my.org.example"
@@ -28,15 +24,6 @@ android {
         targetSdk = 34
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
-    }
-    
-    signingConfigs {
-       create("release") {
-           keyAlias = keyProperties["keyAlias"] as String
-           keyPassword = keyProperties["keyPassword"] as String
-           storeFile = file(keyProperties["storeFile"] as String)
-           storePassword = keyProperties["storePassword"] as String
-       }
     }
 
     buildTypes {
@@ -59,7 +46,6 @@ android {
                     .plus(getDefaultProguardFile("proguard-android-optimize.txt"))
                     .toList().toTypedArray()
             )
-            signingConfig = signingConfigs.getByName("release")
         }
     }
     kotlinOptions {
