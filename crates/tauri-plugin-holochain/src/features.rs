ccc600180afceef25dfe234a29e901e5e3918f16
Matt Gabrenya <matt@buildyourweb.app>
2024-08-06 19:34:12 -0700

    build: better feature names

--- crates/tauri-plugin-holochain/src/features.rs
+++ crates/tauri-plugin-holochain/src/features.rs
@@ -1,15 +1,15 @@
-#[cfg(all(feature = "gossip_arc_clamping_empty", feature = "gossip_arc_clamping_full"))]
+#[cfg(all(feature = "gossip_arc_empty", feature = "gossip_arc_full"))]
 compile_error!(
-    "The `gossip_arc_clamping_empty` and `gossip_arc_clamping_full` features are both enabled, which is an error. Please enable only once."
+    "The `gossip_arc_empty` and `gossip_arc_full` features are both enabled, which is an error. Please enable only one."
 );
-#[cfg(all(feature = "gossip_arc_clamping_empty", feature = "gossip_arc_no_clamping"))]
+#[cfg(all(feature = "gossip_arc_empty", feature = "gossip_arc_normal"))]
 compile_error!(
-    "The `gossip_arc_clamping_empty` and `gossip_arc_no_clamping` features are both enabled, which is an error. Please enable only once."
+    "The `gossip_arc_empty` and `gossip_arc_normal` features are both enabled, which is an error. Please enable only one."
 );
-#[cfg(all(feature = "gossip_arc_clamping_full", feature = "gossip_arc_no_clamping"))]
+#[cfg(all(feature = "gossip_arc_full", feature = "gossip_arc_normal"))]
 compile_error!(
-    "The `gossip_arc_clamping_full` and `gossip_arc_no_clamping` features are both enabled, which is an error. Please enable only once."
+    "The `gossip_arc_full` and `gossip_arc_normal` features are both enabled, which is an error. Please enable only one."
 );
 
-#[cfg(all(not(feature = "gossip_arc_clamping_empty"), not(feature = "gossip_arc_clamping_full"), not(feature = "gossip_arc_no_clamping")))]
-compile_error!("All of the `gossip_arc_clamping_empty`, `gossip_arc_clamping_full`, and `gossip_arc_no_clamping` features are disabled. Please enable one of them.");
+#[cfg(all(not(feature = "gossip_arc_empty"), not(feature = "gossip_arc_full"), not(feature = "gossip_arc_normal")))]
+compile_error!("All of the `gossip_arc_empty`, `gossip_arc_full`, and `gossip_arc_normal` features are disabled. Please enable one of them.");
