d7dfff6f6e4c5c1ad857ad545c1480a395dd6675
Matt Gabrenya <matt@buildyourweb.app>
2024-09-02 20:12:31 -0700

    feat: add tauri-plugin-holochain-foreground-service into repo for ease of development

--- crates/holochain-manager-uniffi/generate-bindings-debug.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-cargo build --package holochain-manager-uniffi && \
-(
-  cargo run --bin uniffi-bindgen generate --library ../../target/debug/libholochain_manager_uniffi.so --language kotlin --out-dir out
-  cargo run --bin uniffi-bindgen generate --library ../../target/debug/libholochain_manager_uniffi.so --language swift --out-dir out
-)
--- crates/holochain-manager-uniffi/src/runtime.rs
+++ crates/holochain-manager-uniffi/src/runtime.rs
@@ -2,6 +2,13 @@ use crate::config::HolochainRuntimeFFIConfig;
 use crate::error::HolochainRuntimeFFIError;
 use crate::types::AppInfoFFI;
 use holochain_manager::{HolochainRuntime, launch::launch_holochain_runtime, utils::vec_to_locked};
+#[macro_use] extern crate log;
+extern crate android_logger;
+
+
+use log::LevelFilter;
+use android_logger::{Config,FilterBuilder};
+
 
 #[derive(uniffi::Object)]
 pub struct HolochainRuntimeFFI {
@@ -12,6 +19,14 @@ pub struct HolochainRuntimeFFI {
 impl HolochainRuntimeFFI {
     #[uniffi::constructor]
     pub async fn launch(passphrase: Vec<u8>, config: HolochainRuntimeFFIConfig) -> Result<Self, HolochainRuntimeFFIError> {
+        android_logger::init_once(
+            Config::default()
+                .with_max_level(LevelFilter::Trace)
+                .with_tag("holochainlog")
+                .with_filter(FilterBuilder::new().parse("debug,hello::crate=trace").build()),
+        );
+        
+        
         let runtime = launch_holochain_runtime(
                 vec_to_locked(passphrase).map_err(|e| HolochainRuntimeFFIError::IOError(e.to_string()))?,
                 config.try_into()?
