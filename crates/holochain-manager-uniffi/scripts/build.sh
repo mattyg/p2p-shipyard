59dac910e37d49f5d07e9eaf24894ad3cc79ea21
Matt Gabrenya <matt@buildyourweb.app>
2024-09-12 17:48:39 -0700

    refactor: only 1 build script for generating bindings

---
+++ crates/holochain-manager-uniffi/scripts/build.sh
@@ -0,0 +1,16 @@
+#/usr/bin/env bash
+
+cargo ndk -o ../tauri-plugin-holochain-foreground-service/android/src/main/jniLibs \
+  --manifest-path ./Cargo.toml \
+  -t arm64-v8a \
+  -t armeabi-v7a \
+  -t x86 \
+  -t x86_64 \
+  build \
+  --release
+
+cargo build --release
+cargo run --bin uniffi-bindgen generate \
+  --library ../../target/release/libholochain_manager_uniffi.so \
+  --language kotlin \
+  --out-dir ../tauri-plugin-holochain-foreground-service/android/src/main/java/
