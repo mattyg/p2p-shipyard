359ff857be4f20ffba5f9b004a58b234e19580eb
Matt Gabrenya <matt@buildyourweb.app>
2024-09-17 20:10:41 -0700

    doc: explanation of rollup config

--- crates/tauri-plugin-holochain-foreground-service/rollup.config.js
+++ crates/tauri-plugin-holochain-foreground-service/rollup.config.js
@@ -1,12 +1,15 @@
 import { readFileSync } from 'fs'
 import { join } from 'path'
 import { cwd } from 'process'
-import typescript from '@rollup/plugin-typescript'
 import terser from '@rollup/plugin-terser'
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-const pkg = JSON.parse(readFileSync(join(cwd(), 'package.json'), 'utf8'))
-
+// NOTE rollup is only used to terse and bundle dependencies for holochain-env bundle
+//
+// It is not used for the tauri-commands bundle because we want ts declaration files,
+// and there is a weird bug where the declaration files for both bundles are included in each bundle, in subdirectories.
+// We are using tsc directly via package.json scripts instead.
+// See https://github.com/rollup/plugins/issues/247
 export default [
   {
     input: 'dist-js-tmp/holochain-env/index.js',
