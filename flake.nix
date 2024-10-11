ee920881772174abd75547de50f5e79178ca2f06
Matt Gabrenya <matt@buildyourweb.app>
2024-10-11 16:15:05 -0400

    fix: bad input

--- flake.nix
+++ flake.nix
@@ -138,8 +138,8 @@
         ./crates/hc-pilot/default.nix
         ./nix/modules/custom-go-compiler.nix
         ./nix/modules/tauri-cli.nix
-        # inputs.hc-infra.outputs.flakeModules.builders
-        inputs.hc-infra.outputs.flakeModules.dependencies
+        # inputs.hc-infra.outputs.flake.flakeModules.builders
+        inputs.hc-infra.outputs.flake.flakeModules.dependencies
       ];
 
       systems = builtins.attrNames inputs.holonix.devShells;
