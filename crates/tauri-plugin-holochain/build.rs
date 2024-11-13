e97d61982cf1cd19da421010741e0e70f158afb1
Matt Gabrenya <matt@buildyourweb.app>
2024-11-13 15:57:48 -0800

    fix: include entitlements in macOS bundle config, to prevent the app from being killed by the os when using just-in-time wasm compilation

--- crates/tauri-plugin-holochain/build.rs
+++ crates/tauri-plugin-holochain/build.rs
@@ -11,4 +11,25 @@ fn main() {
         // .android_path("android")
         // .ios_path("ios")
         .build();
+
+    // Add an entitlement on macOS builds to ensure
+    // the app is allowed to use just-in-time wasm compilation
+    #[cfg(target_os = "macos")]
+    {
+        tauri_plugin::mobile::update_entitlements(|entitlements| {
+            entitlements.insert(
+                "com.apple.security.cs.allow-jit".into(),
+                true
+            );
+            entitlements.insert(
+                "com.apple.security.cs.allow-unsigned-executable-memory".into(),
+                true
+            );
+            entitlements.insert(
+                "com.apple.security.cs.disable-library-validation".into(),
+                true
+            );
+        })
+        .expect("failed to update entitlements");
+    }
 }
