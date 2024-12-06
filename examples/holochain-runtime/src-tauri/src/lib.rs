5f3883c11e1a0bb51bea4798ef5db5fe3a03ab65
guillem.cordoba <guillem.cordoba@gmail.com>
2024-12-06 20:12:22 +0100

    Fixed vec_to_locke

--- examples/holochain-runtime/src-tauri/src/lib.rs
+++ examples/holochain-runtime/src-tauri/src/lib.rs
@@ -1,7 +1,6 @@
-use lair_keystore::dependencies::sodoken::{BufRead, BufWrite};
 use std::collections::HashMap;
 use std::path::PathBuf;
-use tauri_plugin_holochain::{HolochainExt, HolochainPluginConfig};
+use tauri_plugin_holochain::{HolochainExt, HolochainPluginConfig, vec_to_locked};
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -77,22 +76,5 @@ fn holochain_dir() -> PathBuf {
         )
         .expect("Could not get app root")
         .join("holochain")
-    }
-}
-
-fn vec_to_locked(mut pass_tmp: Vec<u8>) -> std::io::Result<BufRead> {
-    match BufWrite::new_mem_locked(pass_tmp.len()) {
-        Err(e) => {
-            pass_tmp.fill(0);
-            Err(e.into())
-        }
-        Ok(p) => {
-            {
-                let mut lock = p.write_lock();
-                lock.copy_from_slice(&pass_tmp);
-                pass_tmp.fill(0);
-            }
-            Ok(p.to_read())
-        }
     }
 }
